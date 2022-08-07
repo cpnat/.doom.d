4aa4eacdad9c4ae89e4b238e4c8212f3daadb3fe
Colin Nattrass <cpnattrass@googlemail.com>
2022-08-07 11:18:07 +0200

    Setup Metals for Scala

--- config.el
+++ config.el
@@ -161,6 +161,34 @@
 (setenv "JAVA_HOME" java-home-param)
 (setq lsp-java-java-path lsp-java-java-path-param)
 
+;; Enable scala-mode and sbt-mode
+(use-package scala-mode
+  :mode "\\.s\\(cala\\|bt\\)$")
+
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
+
+;; Enable nice rendering of diagnostics like compile errors.
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook (scala-mode . lsp)
+  :config (setq lsp-prefer-flymake nil))
+
+(use-package lsp-ui)
+
+;; Add company-lsp backend for metals
+(use-package company-lsp)
+
 (use-package lsp-sourcekit
   :after lsp-mode
   :config
